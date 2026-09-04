acdc6102ec50bad0f9328337c4a9b0995123dfe6
Manuka Abeysekara <it24100043@sliit.lk>
2026-09-04 14:43:18 +0530

    .

--- frontend/Project/vite.config.js
+++ frontend/Project/vite.config.js
@@ -5,4 +5,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['frontend-production-e0611.up.railway.app'],
+  },
 })
